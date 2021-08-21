2a18a52bb005ebdb59141928a1acfb1a50049f78
mayurkalekar087 <mayurkalekar087@gmail.com>
2021-08-21 13:52:38 +0530

    feat:Stock Account Management

---
+++ StockManagement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StockManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stock stock = new Stock();
+            Console.WriteLine("Welcome to Stock Account Management!");
+            Console.WriteLine("************************************");
+            Console.WriteLine("Enter Number Of Stocks:-");
+            int NumberOfStocks = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= NumberOfStocks; i++)
+            {
+                Console.WriteLine("Enter ShareName:-");
+                string ShareName = Console.ReadLine();
+
+                Console.WriteLine("Enter Total Number Of Shares:-");
+                int NoOfShares = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Price Per Share:-");
+                int PricePerEachShare = int.Parse(Console.ReadLine());
+
+                stock.Stockvalue(NoOfShares, PricePerEachShare, ShareName);
+            }
+        }
+    }
+}
+
---
+++ StockManagement/Stock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockManagement
+{
+   public class Stock
+    {
+        public int totalshare = 0;
+        public int totalPrice = 0;
+        public void Stockvalue(int p, int q, string r )
+        {
+            int stockPrice = p * q;
+            Console.WriteLine("The Price for stock " + r + " shares is :  " + stockPrice);
+            totalPrice += (p * q);
+            Console.WriteLine("the total price of the stock is  " + totalPrice);
+        }
+    }
+}
